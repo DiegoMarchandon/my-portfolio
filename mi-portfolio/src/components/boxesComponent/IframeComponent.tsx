dbcb33d6d5608523c6f96e05c54334ca8d736706
DiegoMarchandon <diegomarc2020@gmail.com>
2026-05-27 23:35:25 -0300

    Refactorización reemplazando codigo de iframes por un componente

---
+++ mi-portfolio/src/components/boxesComponent/IframeComponent.tsx
@@ -0,0 +1,76 @@
+import styles from '../../styles/boxes.module.css';
+import boxStyles from '../../styles/boxes/projects.module.css';
+
+interface iframeComponentProps {
+    iframeTitle: string;
+    iframeSrc: string;
+    iframeDescription: string;
+    tecnologiasUsadas: Array<string|string[]>;
+    fuentesUsadas: Array<string|string[]>;
+}
+
+export default function IframeComponent({iframeTitle, iframeSrc,iframeDescription,tecnologiasUsadas,fuentesUsadas}: iframeComponentProps){
+    return(
+        <div className={`${boxStyles.iframeContainer}`}>
+            <div className={`${boxStyles.titleWrapper}`}>
+                <h3><a href={iframeSrc}>{iframeTitle}</a></h3>
+            </div>
+            <div className={`${boxStyles.iframeWrapper}`}>
+                <iframe
+                className={`${boxStyles.iframe}`}
+                src={iframeSrc}
+                loading="lazy"
+                title="Vista previa del proyecto"
+                >
+                </iframe>
+            </div>
+            <div className={`${boxStyles.iframeInfo}`}>
+                <div className={`${boxStyles.iframeDescription}`}>
+                    <p>{iframeDescription}</p>
+                </div>
+                <div className={`${boxStyles.iframeTechnologies}`}>
+                    <h3>Tecnologías utilizadas</h3>
+                    {
+                        <ul>
+                            {
+                            tecnologiasUsadas.map((item,index)=>{
+                                if(Array.isArray(item)){
+                                    return (
+                                            <ul key={index}>
+                                                {
+                                            item.map((itemString,subIndex)=>(
+                                                <li key={`${index}-${subIndex}`}>{itemString}</li>
+                                            ))}
+                                        </ul>
+                                        )
+                                        
+                                }else{
+                                    return <li key={index}>{item}</li>;
+                                }
+                            })}
+                        </ul>
+                    }
+                    <h3>Fuentes utilizadas</h3>
+                    <ul>
+                        {
+                            fuentesUsadas.map((item,index)=>{
+                                if(Array.isArray(item)){
+                                    return (
+                                            <ul key={index}>
+                                                {
+                                            item.map((itemString,subIndex)=>(
+                                                <li key={`${index}-${subIndex}`}>{itemString}</li>
+                                            ))}
+                                        </ul>
+                                        )
+                                        
+                                }else{
+                                    return <li key={index}>{item}</li>;
+                                }
+                            })}
+                    </ul>
+                </div>
+            </div>
+        </div>
+    );
+}
